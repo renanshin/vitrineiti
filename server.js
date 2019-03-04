fb7ee0bc8784aa0a3eaa4b2ee6177baa4beb013d
RenanShin <renanshin@gmail.com>
2019-03-04 19:22:58 -0300

    Procfile commit

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const express = require(‘express’);
-const http = require(‘http’)
-const path = require(‘path’);
-
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
 const app = express();
-
-app.use(express.static(path.join(__dirname, ‘dist’)));
-
-app.get(‘*’, (req, res) => {
-res.sendFile(path.join(__dirname + ‘/dist/index.html’));
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
-
-const port = process.env.PORT || 3000;
-app.set(‘port’, port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log(‘running’));
+ 
+app.listen(process.env.PORT || 8080);
